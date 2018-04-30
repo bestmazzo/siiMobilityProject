520fb90f567425d66e118a002395e0f9de2ef091
Matteo Mazzoni <matteo@bestmazzo.it>
2018-04-30 19:54:13 +0200

    v0.9 - stubbed global architecture - prepared endpoints - allocated methods TBD later on

---
+++ API20-SmartCity-API-to-traffic-manager/app/Http/Controllers/SuggestionController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Routing\Controller as BaseController;
+
+
+class SuggestionController extends BaseController
+{
+ public function index(){
+		return "TBD";
+}
+}
